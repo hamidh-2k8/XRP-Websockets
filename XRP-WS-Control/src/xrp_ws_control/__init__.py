37afbe09bf7bca6bb8d2b238d4bf1ec4ac13bb47
hamidh-2k8 <hamidh.2k8@gmail.com>
2026-08-27 10:52:04 -0400

    Added keyboard control

--- XRP-WS-Control/src/xrp_ws_control/__init__.py
+++ XRP-WS-Control/src/xrp_ws_control/__init__.py
@@ -4,17 +4,32 @@ import websockets, pygame, time
 
 pygame.init()
 
+global useJoysticks
+useJoysticks = False
+
 async def main():
-    if pygame.joystick.get_count() == 0:
-        print("No joystick detected.")
-        return
+    if pygame.joystick.get_count() > 0:
+        global useJoysticks
+        useJoysticks = True
 
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-    num_axes = joystick.get_numaxes()
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+        num_axes = joystick.get_numaxes()
 
-    print(f"Joystick detected: {joystick.get_name()}")
-    print(f"Number of axes: {num_axes}")
+        print(f"Joystick detected: {joystick.get_name()}")
+        print(f"Number of axes: {num_axes}")
+    else:
+        print("Using keyboard controls (WASD or Arrow Keys)")
+        background_colour = (234, 212, 252)
+
+        screen = pygame.display.set_mode((300, 300))
+
+        pygame.display.set_caption('Keyboard Control')
+
+        screen.fill(background_colour)
+
+        pygame.display.flip()
+        
     print("Enter XRP WSS URL\nws://", end="")
     ip = input()
 
@@ -27,18 +42,26 @@ async def main():
     while True:
         pygame.event.pump()
         
-        power = -joystick.get_axis(1)
-        turn = joystick.get_axis(2)
+        if useJoysticks:
+            power = -joystick.get_axis(1)
+            turn = joystick.get_axis(2)
+            
+            left_shoulder = joystick.get_button(4)
+            right_shoulder = joystick.get_button(5)
         
-        left_shoulder = joystick.get_button(4)
-        right_shoulder = joystick.get_button(5)
         
-        if right_shoulder:
-            servo = 255
-        elif left_shoulder:
-            servo = 127
+            if right_shoulder:
+                servo = 255
+            elif left_shoulder:
+                servo = 127
+            else:
+                servo = 0
         else:
-            servo = 0
+            keys=pygame.key.get_pressed()
+            
+            power = 1 if keys[pygame.K_UP] else -1 if keys[pygame.K_DOWN] else 0
+            turn = 1 if keys[pygame.K_RIGHT] else -1 if keys[pygame.K_LEFT] else 0
+            servo = 127 if keys[pygame.KMOD_SHIFT] else 0
 
         data = {
             "power": power,
